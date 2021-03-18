556ada29d298c72ae7d6e48904e97bdd3aca2330
rahulkalita-eng <79743312+rahulkalita-eng@users.noreply.github.com>
2021-03-18 20:07:55 +0530

    Add files via upload

---
+++ prog02.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+   float x, y, result;
+   cout<<"Enter two numbers: ";
+   cin>>x>>y;
+   result = x * y;
+   cout<<"Product = "<<result;
+   return 0;
+}
+
